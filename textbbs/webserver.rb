b0b017c43420973f5fad2ea027cba0c46d5e3876
mizutaki <mizutaki4@gmail.com>
2014-08-28 22:13:35 +0900

    vaidate imput item for now.

--- textbbs/webserver.rb
+++ textbbs/webserver.rb
@@ -1,5 +1,5 @@
 require 'webrick'
-
+require 'pry'
 server_config = {
     :Port => 8099,
     :DocumentRoot => '.',
@@ -10,12 +10,29 @@ server_config = {
 server = WEBrick::HTTPServer.new(server_config)
 
 server.mount_proc("/write") { |req, res|
-    template = ERB.new(File.read('erb/write.erb'))
-    res.body << template.result(binding)
+    #binding.pry
+    begin
+        validate_parameter req.query
+        template = ERB.new(File.read('erb/write.erb'))
+        res.body << template.result(binding)    
+    rescue ArgumentError => ae
+        template = ERB.new(File.read('erb/error.erb'))
+        res.body << template.result(binding)
+    end
 }
 
 trap(:INT) {
     server.shutdown
 }
+
+def validate_parameter(query)
+    if query['name'].empty?
+        raise ArgumentError.new("名前を入力して下さい。") 
+    end
+    p query
+    if query['textarea'].empty?
+        raise ArgumentError.new("本文を入力して下さい。")
+    end
+end
 
 server.start
