61970ff4feb3fe84167129eddc6b44e87acf98c3
mizutaki4 <mizutaki4@gmail.com>
2015-03-12 13:36:05 +0900

    trail commit writer csv file

--- file_operation_tools/output_contents_info.rb
+++ file_operation_tools/output_contents_info.rb
@@ -1,18 +1,23 @@
 # coding: utf-8
+require 'csv'
+
 current_dir = File.expand_path(File.dirname(__FILE__))
 dirs = Dir.glob("#{current_dir}/*")
 
 def current_content(dirs)
   dirs.each do |content|
-  
-    if FileTest.file?(content)
-      puts "FILE" 
-      puts content
-      puts "#{File.size(content)}byte"
-    else
-      puts "DIR"
-      puts content
-      current_content(Dir.glob("#{content}/*")) 
+    CSV.open('test.csv', 'w+') do |writer|
+      if FileTest.file?(content)
+        puts "FILE" 
+        puts content
+        puts "#{File.size(content)}byte"
+        writer << ["#{content}", "#{File.size(content)}"]
+      else
+        puts "DIR"
+        puts content
+        writer << ["#{content}", "0"]
+        current_content(Dir.glob("#{content}/*")) 
+      end
     end
   end
 end
