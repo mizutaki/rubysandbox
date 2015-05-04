3933b2008262218817b2fdf6d42f7d78583fcb3a
mizutaki <mizutaki4@gmail.com>
2015-05-04 10:22:52 +0900

    align the indentation

--- fullpath_generator/fullpath_generator.rb
+++ fullpath_generator/fullpath_generator.rb
@@ -1,23 +1,23 @@
 require 'rbconfig'
 
 module FullPathGenerator
-	include RbConfig
+  include RbConfig
 	
-	#フルパスを作成する
-	def generate_fullpath(hierarchy)
-		concatenate_string = ""
-		hierarchy.times {
-			concatenate_string << random_character
-			concatenate_string << "/"
-		}
+  #フルパスを作成する
+  def generate_fullpath(hierarchy)
+    concatenate_string = ""
+    hierarchy.times {
+      concatenate_string << random_character
+      concatenate_string << "/"
+    }
     puts concatenate_string
   end
 
   private
     #ランダムな文字列を作成する（10文字）
     def random_character
-		  return ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(10).join
-	  end
+      return ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(10).join
+    end
 
     #TODO 実行環境の判定を行って、環境毎のフルパスを作成できるようにする
     def judge_enviroment
