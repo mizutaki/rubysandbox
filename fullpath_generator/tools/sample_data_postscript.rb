df7447980d53bc4b1727dc9099ec5d5e644d409b
mizutaki <mizutaki4@gmail.com>
2015-05-24 16:16:23 +0900

    made parallel computation, and transaction speed became early 1000 word 3minute

--- fullpath_generator/tools/sample_data_postscript.rb
+++ fullpath_generator/tools/sample_data_postscript.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv' 
 require 'yaml'
-
+require 'parallel'
 #サンプルデータ追記用のスクリプト
 #wikipedia(英語版、日本語版)にランダムにアクセスして単語を取得して、csvファイルに追記する
 
@@ -11,8 +11,9 @@ wiki_url = ["http://wikipedia.org/wiki/Special:Randompage", "http://ja.wikipedia
 conf = YAML.load_file('conf.yml')
 number = conf['number'].to_i
 words = []
-number.times{
-
+arr = []
+number.times{|i| arr << i}
+Parallel.each(arr, in_threads:10) {|i|
   url = wiki_url.sample(1).first
   charset = nil
   html = open(url) do |f|
